75051f5d000130233e0151627454b8d03a8f4985
QueencessMay <munsayac.queencessmay@gmail.com>
2023-06-12 23:50:18 +0800

    Adjust screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'lg': '900px',
+      },
+    },
   },
   plugins: [],
 };
