156c9fd6e5c3c1456dfa65b10090d8451fc2a68e
QueencessMay <munsayac.queencessmay@gmail.com>
2023-06-13 12:37:09 +0800

    Updated Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,17 +10,18 @@ const Carousel = () => {
     ]
 
     const [index, setIndex] = useState(0);
-    const [isForward, setForward] = useState(true);
     const timeoutRef = useRef(null);
 
     const goForward = () => {
-        setForward(true);
         setIndex((index + 1) % images.length);
     }
 
     const goBack = () => {
-        setForward(false);
         setIndex((index - 1 + images.length) % images.length);
+    }
+
+    const goToSlide = (index) => {
+        setIndex(index);
     }
 
     function resetTimeout() {
@@ -36,16 +37,31 @@ const Carousel = () => {
     }, [index]); 
 
     return (
-        <div className='relative flex w-full h-full overflow-hidden animate-waving-hand'>
-            <div className={`duration-500 ease-in-out`}>
-                <img src={images[index]} className="w-full h-full object-cover" />
+        <div className='relative flex w-full h-full overflow-hidden rounded-2xl'>
+            <div 
+                className='flex transition-transform ease-out duration-300' 
+                style={{ transform: `translateX(-${index * 100}%)`}}
+            >
+                {images.map((image) => (<img src={image} className='object-cover' />))}
             </div>
-            <div className='absolute top-1/2 p-1 flex justify-between align-middle w-full transform -translate-y-1/2'>
-                <div className='text-white text-4xl md:text-5xl' onClick={goBack}>
+            <div className='absolute inset-2 flex justify-between items-center'>
+                <button className='text-white text-4xl md:text-5xl transition-all hover:scale-150' onClick={goBack}>
                     <ion-icon name="caret-back-circle"></ion-icon>
-                </div>
-                <div className='text-white text-4xl md:text-5xl' onClick={goForward}>
+                </button>
+                <button className='text-white text-4xl md:text-5xl transition-all hover:scale-150' onClick={goForward}>
                     <ion-icon name="caret-forward-circle"></ion-icon>
+                </button>
+            </div>
+            <div className='absolute inset-x-0 bottom-2 sm:bottom-5'>
+                <div className='flex justify-center gap-2'>
+                    {
+                        images.map((_, i) => (
+                            <button 
+                            key={i} 
+                            onClick={() => goToSlide(i)}
+                            className={`w-3 h-1.5 sm:w-5 rounded-sm bg-white transition-all ${index === i ? 'w-6 sm:w-8' : 'opacity-60'}`}></button>
+                        ))
+                    }
                 </div>
             </div>
         </div>
